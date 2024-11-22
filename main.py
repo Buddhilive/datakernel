4829abbafbc248ab9e35cbcb1055d338015529af
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-22 19:22:03 +0530

    view dataset

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.write("# Ayubowan!")
 x = st.text_input("Type anything...")
 st.write(f"You typed: {x}")
+
+population_data = pd.read_csv("data/population.csv")
+st.write(population_data)
