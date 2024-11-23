e2f7022d99b1bd830aaf611617daa9387fc5ddb9
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-23 16:48:17 +0530

    Switch to california housing data

--- pages/9_Housing.py
+++ pages/9_Housing.py
@@ -5,19 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 st.write("""
-# Boston House Price Prediction App
+# California House Price Prediction App
 
-This app predicts the **Boston House Price**!
+This app predicts the **California House Price**!
 """)
 st.write('---')
 
-# Loads the Boston House Price Dataset
+# Loads the California House Price Dataset
 from sklearn.datasets import fetch_california_housing  # For California housing
-# or
-from sklearn.datasets import fetch_openml  # For Ames housing
-housing = fetch_openml(name="house_prices", as_frame=True)
+
+housing = fetch_california_housing(as_frame=True)
 X = pd.DataFrame(housing.data, columns=housing.feature_names)
-Y = pd.DataFrame(housing.target, columns=["MEDV"])
+Y = pd.DataFrame(housing.target)
 
 # Sidebar
 # Header of Specify Input Parameters
@@ -26,40 +25,36 @@ st.sidebar.header('Specify Input Parameters')
 st.write(X)
 
 def user_input_features():
-    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
+    # Using sliders for each feature in the California housing dataset
+    MedInc = st.sidebar.slider('MedInc', X.MedInc.min(), X.MedInc.max(), X.MedInc.mean())
+    HouseAge = st.sidebar.slider('HouseAge', X.HouseAge.min(), X.HouseAge.max(), X.HouseAge.mean())
+    AveRooms = st.sidebar.slider('AveRooms', X.AveRooms.min(), X.AveRooms.max(), X.AveRooms.mean())
+    AveBedrms = st.sidebar.slider('AveBedrms', X.AveBedrms.min(), X.AveBedrms.max(), X.AveBedrms.mean())
+    Population = st.sidebar.slider('Population', X.Population.min(), X.Population.max(), X.Population.mean())
+    AveOccup = st.sidebar.slider('AveOccup', X.AveOccup.min(), X.AveOccup.max(), X.AveOccup.mean())
+    Latitude = st.sidebar.slider('Latitude', X.Latitude.min(), X.Latitude.max(), X.Latitude.mean())
+    Longitude = st.sidebar.slider('Longitude', X.Longitude.min(), X.Longitude.max(), X.Longitude.mean())
+
+    # Constructing the input data as a DataFrame
+    data = {
+        'MedInc': MedInc,
+        'HouseAge': HouseAge,
+        'AveRooms': AveRooms,
+        'AveBedrms': AveBedrms,
+        'Population': Population,
+        'AveOccup': AveOccup,
+        'Latitude': Latitude,
+        'Longitude': Longitude
+    }
     features = pd.DataFrame(data, index=[0])
     return features
 
-# df = user_input_features()
+
+df = user_input_features()
 
 # Main Panel
 
-""" # Print specified input parameters
+# Print specified input parameters
 st.header('Specified Input parameters')
 st.write(df)
 st.write('---')
@@ -87,4 +82,4 @@ st.write('---')
 
 plt.title('Feature importance based on SHAP values (Bar)')
 shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight') """
+st.pyplot(bbox_inches='tight')
