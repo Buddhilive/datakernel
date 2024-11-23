e7f22085161eaee64df0e9a6f065628258287557
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-23 17:28:35 +0530

    Update Home page

--- Home.py
+++ Home.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
-st.write("# Ayubowan!")
+st.write(
+    """
+    # Ayubowan to Datakernel!
+
+This project contains Streamlit based Data Science apps.
+
+Following apps are based on this [YouTube tutorial](https://youtu.be/JwSS70SZdyM?si=NT2Y6tPuRVh6LYAq):
+
+1. Stock Price
+2. Bio Informatics
+3. EDA Basketball
+4. EDA Football
+5. SP500
+6. Crypto Currency
+7. Iris Classification
+8. Penguin Classification
+
+> ### Note
+> Housing app was changed to use California dataset.
+
+Rest of the apps are personal experiments
+         """
+)
