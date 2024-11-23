6c50c443767e9d7f417efd76776abd54cf7fa3b8
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-23 11:57:35 +0530

    update home page

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 import streamlit as st
-import pandas as pd
 
 st.write("# Ayubowan!")
-x = st.text_input("Type anything...")
-st.write(f"You typed: {x}")
-
-population_data = pd.read_csv("data/population.csv")
-st.write(population_data)
