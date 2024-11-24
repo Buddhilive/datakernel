eb0e254b5a6c673a27d3d0a80bdde72e2bda8b8c
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-24 19:15:45 +0530

    Explore population data

--- pages/10_World_Population.py
+++ pages/10_World_Population.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-st.write("# World Population")
+st.title("World Population")
+st.write(
+    """
+Analysing world population data
+"""
+)
 
 population_data = pd.read_csv("data/population.csv")
-st.write(population_data)
+
+st.header("Data Summary")
+st.write(population_data.head())
+
+st.header("Select data by Country")
+selected_country = st.selectbox("Country", sorted(population_data["Country (or dependency)"]))
+filtered_df = population_data[population_data['Country (or dependency)'] == selected_country]
+st.write(filtered_df)
