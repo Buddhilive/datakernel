9c75eb41fe6bf4485beabfbe24485c21441352d8
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-11-24 20:30:49 +0530

    Clean "Med. Age" Column

--- pages/10_World_Population.py
+++ pages/10_World_Population.py
@@ -27,6 +27,9 @@ population_data["Yearly Change"] = population_data["Yearly Change"].str.replace(
 population_data["Fert. Rate"] = population_data["Fert. Rate"].replace('N.A.', np.nan).astype(float)
 population_data["Fert. Rate"].fillna(population_data["Fert. Rate"].median(), inplace = True)
 
+population_data["Med. Age"] = population_data["Med. Age"].replace('N.A.', np.nan).astype(float)
+population_data["Med. Age"].fillna(population_data["Med. Age"].median(), inplace = True)
+
 population_data["Urban Pop %"] = population_data["Urban Pop %"].str.replace(' %', '').replace('N.A.', np.nan).astype(float)
 population_data["Urban Pop %"].fillna(population_data["Urban Pop %"].median(), inplace = True)
 
